d408b18dfddfca56236cf16f5f9c59be603b3a89
ikaraoz <i.karaoz@outlook.com>
2020-05-17 00:10:59 +0100

    With comments and self-made calculator APK

--- src/test/java/appium/CalculatorTest.java
+++ src/test/java/appium/CalculatorTest.java
@@ -1,15 +1,18 @@
+
+
 package appium;
+
+import java.net.URL;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
-public class AppiumSetup {
+public class CalculatorTest {
+
     static AppiumDriver driver;
     //Both androiddriver and the iosdriver extends from AppiumDriver.
     //If the tests will be used for two different platforms,
@@ -28,15 +31,9 @@ public class AppiumSetup {
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel API 29");
-        caps.setCapability("appPackage", "io.appium.android.apis"); //these two information come from the info of the class
-     //   caps.setCapability("appActivity", ".ApiDemos");
-      caps.setCapability("appActivity", ".app.Animation");
-
-/*
-The package and page information can be found from the info details of the app
-In mobile app, each page is an activity, and we find and set these as capacbilities
-to perform actions on them.
- */
+        caps.setCapability("appPackage", "com.google.android.calculator"); //these two information come from the info of the class
+        //   caps.setCapability("appActivity", ".ApiDemos");
+        caps.setCapability("appActivity", "com.android.calculator2.Calculator");
         URL url = new URL("http://localhost:4723/wd/hub");
         /*
         This is the servers' url.
@@ -45,22 +42,5 @@ to perform actions on them.
         URL url3 = new URL("http://0.0.0.0:4723/wd/hub");
 
         driver = new AndroidDriver(url, caps);
-        /*
-        This is initialising the driver.
-         */
-
-        driver.findElementByAccessibilityId("Fade in").click();
-        Thread.sleep(500);
-        driver.findElementByAccessibilityId("List dialog").click();
-        Thread.sleep(500);
-        driver.findElementByXPath("//*[@text='Command one']").click();
-
-        /*
-        Pretty much similar to what we are doing with the selenium. Instead of initialising
-        websdriver, we are running AppiumDriver.
-        Finding the elements is the same. You find the element and then do operations on it.
-        To find the id, or to inspect the element, we go and find it through the Appium session.
-         */
-
     }
 }
